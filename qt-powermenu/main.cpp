30874f1256a8c0415daaac1ed6107d793b9ca144
K0SS4 <github@k0ss4.xyz>
2023-10-31 12:46:32 +0100

    Check if application is already running before starting

--- qt-powermenu/main.cpp
+++ qt-powermenu/main.cpp
@@ -1,9 +1,23 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QProcess>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
+    QProcess* process = new QProcess();
+    process->start("pgrep", {"qt-powermenu"});
+    process->waitForFinished();
+
+    if(process->readAllStandardOutput().split('\n').length() > 2) //Check if application is already running
+    {
+        delete process;
+        return 0;
+    }
+
+    delete process;
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
